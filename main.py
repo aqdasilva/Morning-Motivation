c332b69f0377ca124a0116aa207adcaa8c3a40b0
aqdasilva <antqdasilva@gmail.com>
2023-01-04 18:43:02 -0500

    added all days of the week to randomize thru certain numbers of the lines of the text file. Next: add in holidays

--- main.py
+++ main.py
@@ -2,22 +2,79 @@ import re
 import smtplib
 import random
 import re
+from datetime import datetime
 from email.mime.text import MIMEText
 
 filename = "quotes.txt"
 
-def get_random_quote(filename):
+
+def function_for_monday(filename):
     with open(filename, 'r', encoding="utf8") as f:
-        #reads lines in file
         lines = f.readlines()
-        # Use a list comprehension to extract the lines that are in quotation marks
-        quotes = [line for line in lines if re.search('"(.+)"', line)]
+        return random.choice(lines[:20])
 
-        # Select a random line
-        line = random.choice(quotes)
 
-        # return the line
-        return (line)
+def function_for_tuesday(filename):
+    with open(filename, 'r', encoding="utf8") as f:
+        lines = f.readlines()
+        return random.choice(lines[20:41])
+
+def function_for_wednesday(filename):
+    with open(filename, 'r', encoding="utf8") as f:
+        lines = f.readlines()
+        return random.choice(lines[42:53])
+
+def function_for_thursday(filename):
+    with open(filename, 'r', encoding="utf8") as f:
+        lines = f.readlines()
+        return random.choice(lines[54:65])
+
+def function_for_friday(filename):
+    with open(filename, 'r', encoding="utf8") as f:
+        lines = f.readlines()
+        return random.choice(lines[66:77])
+
+def function_for_saturday(filename):
+    with open(filename, 'r', encoding="utf8") as f:
+        lines = f.readlines()
+        return random.choice(lines[78:89])
+
+def function_for_sunday(filename):
+    with open(filename, 'r', encoding="utf8") as f:
+        lines = f.readlines()
+        return random.choice(lines[90:100])
+
+
+def get_random_quote(filename):
+    # with open(filename, 'r', encoding="utf8") as f:
+    #     # reads lines in file
+    #     lines = f.readlines()
+    #     # Use a list comprehension to extract the lines that are in quotation marks
+    #     quotes = [line for line in lines if re.search('"(.+)"', line)]
+    #
+    #     # Select a random line
+    #     line = random.choice(quotes)
+    #
+    #     # return the line
+    #     return (line)
+    from datetime import datetime
+    current_day = datetime.today().weekday()
+    if current_day == 0:
+        return function_for_monday(filename)
+    elif current_day == 1:
+        return function_for_tuesday(filename)
+    elif current_day == 2:
+        return function_for_wednesday(filename)
+    elif current_day == 3:
+        return function_for_thursday(filename)
+    elif current_day == 4:
+        return function_for_friday(filename)
+    elif current_day == 5:
+        return function_for_saturday(filename)
+    elif current_day == 6:
+        return function_for_sunday(filename)
+    else:
+        return "you stoopid fix this"
 
 
 # Set the email parameters
